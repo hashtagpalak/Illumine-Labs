a42ec34cc0126d90b05a216bd920e5081254323f
Palak Srivastava <61238006+hashtagpalak@users.noreply.github.com>
2022-03-03 14:28:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
+//import ReactDOM from 'react-dom';
 import Tables01 from './components/table01';
+import Tables02 from './components/table02';
+import Button from './components/button';
 
 function App() {
 
@@ -7,10 +10,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h4>Assignment - 01</h4>
+        <h5>Table using functional component</h5>
         <Tables01/>
+        <br></br>
+        <Button title={'Assign'}/>
+        <h5>Table using class component</h5>
+        <Tables02/>
       </header>
     </div>
   );
 }
 
 export default App;
+
