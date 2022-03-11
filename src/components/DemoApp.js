8ea56b47c06859fd6f3b940b7122e8461cb5dd29
Palak Srivastava <61238006+hashtagpalak@users.noreply.github.com>
2022-03-11 18:09:01 +0530

    Add files via upload
    Connecting firebase to the project

---
+++ src/components/DemoApp.js
@@ -0,0 +1,95 @@
+import React, {Component} from "react";
+import Button from "./Button";
+import firebase from "./fbConfig";
+
+
+class DemoApp extends Component{
+    constructor(){   
+      super();
+      this.state = {
+          details1: [{name: "Abc", email:'Abc@gmail.com', phone: '12345XXX', address: 'Place Abc', },
+          {name: "Abc", email:'Abc@gmail.com', phone: '12345XXX', address: 'Place Abc' },
+          {name: "Abc", email:'Abc@gmail.com', phone: '12345XXX', address: 'Place Abc'}
+        ],
+
+         details2: [{name: 'Pqrst', email:'pqrst@gmail.com', phone: 'XXX67890', address: 'Place Pqrst', },
+          {name: 'Pqrst', email:'pqrst@gmail.com', phone: 'XXX67890', address: 'Place Pqrst' },
+          {name: 'Pqrst', email:'pqrst@gmail.com', phone: 'XXX67890', address: 'Place Pqrst'}
+        ],
+        tableDetails:[{name: "Abc", email:'Abc@gmail.com', phone: '12345XXX', address: 'Place Abc', },
+        {name: "Abc", email:'Abc@gmail.com', phone: '12345XXX', address: 'Place Abc' },
+        {name: "Abc", email:'Abc@gmail.com', phone: '12345XXX', address: 'Place Abc'}
+      ],
+        button1: 'Edit',
+        button2: 'Delete',
+        buttonTitle: 'Show Table 2'
+      }
+      
+    }
+
+    clickState() {
+      const {buttonTitle, details1, details2} = this.state
+      if (buttonTitle === 'Show Table 2'){
+        
+        this.setState({
+          buttonTitle: 'Show Table 1',
+          tableDetails: details2
+        })
+      }
+      else {
+       
+      this.setState({
+        buttonTitle: 'Show Table 2',
+        tableDetails: details1
+      })
+      }
+    };
+
+    componentDidMount(){
+      console.log('database',firebase.database())
+      
+      firebase.database().ref('users/' + 'abcd12345').set({
+        username:"abcdef",
+        email:"email@email.com",
+      });
+    }
+  
+    render(){
+      const {buttonTitle,tableDetails,button1,button2} = this.state
+        return(
+        <>
+          <table>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone No.</th>
+                <th>Address</th>
+                <th>Actions</th>
+              </tr>
+              
+              {
+                tableDetails.map(item => 
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                  <td>{item.address}</td>
+                  <span>
+                    <Button title={button1}/>
+                    <Button title={button2}/>
+                  </span>   
+                </tr>
+                )
+              }
+            </table>
+
+        <br></br>
+        <span>
+          <Button onButtonClick={() => this.clickState()}title={buttonTitle}/>
+        </span>
+        </>
+        );
+      }
+  }
+
+  export default DemoApp;
